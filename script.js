852720282cf71533608d2f54413a4dc602a1ebf7
Yuri <patrocinio1959@gmail.com>
2023-09-06 00:27:46 -0300

    comit dos exercicios feitos

---
+++ script.js
@@ -0,0 +1,97 @@
+/* 
+    1- Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+    2-Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+    3- Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+    4- Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+    5- Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+    6- Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+    7- Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+    8- Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+    9- Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+    10- Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+    
+
+
+*/ 
+
+/* 1)
+alert("Hello World!");*/
+
+/* 2)
+
+const numeroUm = 400
+const numeroDois = 20
+
+const sum = numeroUm + numeroDois
+alert(`${sum}`);*/
+
+/*3)
+let variable = 40
+
+if (typeof variable == 'number') {
+    alert("É um número")
+} else {
+    alert("Não é um número")    
+}*/
+
+/*4)
+let variable = 'Rio De Janeiro'
+
+if (typeof variable == 'string'){
+    alert("É uma string!")
+} else {
+    alert("Não é uma string!")
+}*/
+
+/*5)
+let variable = true
+if (typeof variable == 'boolean') {
+    alert("É um boleano!")
+} else {
+    alert("Não é um boleano!")
+}*/
+
+/*6)
+const numberOne  = 400
+const numberTwo = 20
+
+const result = numberOne - numberTwo
+
+alert(result)
+*/
+
+/*7)
+const numberOne  = 400
+const numberTwo = 20
+const result = numberOne * numberTwo
+alert(`a multiplicação é ${result}.`)
+
+*/ 
+
+/*8)
+const numberOne = 400
+const numberTwo = 20
+const result = numberOne/numberTwo
+
+alert(`A divisão entre o numberOne pelo numberTwo é ${result}`)
+*/
+
+/*9)
+const variable = 1970
+if (variable %2 == 0) {
+    alert("É par!")
+} else {
+    alert("Não é par.")
+}*/
+
+/*10)
+const variable = 1920
+if (variable %2 !=0) {
+    alert(`O número é impar!`)
+} else {
+    alert(`Não é impar`)
+}
+*/
+
+
+
